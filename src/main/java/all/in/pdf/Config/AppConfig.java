7563cfc5671010addeba711b09e416ec1daf2c5c
mharmouch <mohcine.harmouch@docaposte.fr>
2021-05-16 17:59:07 +0100

    add App config

---
+++ src/main/java/all/in/pdf/Config/AppConfig.java
@@ -0,0 +1,24 @@
+package all.in.pdf.Config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EnableAsync
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppConfig {
+    private List<String> authorizedRedirectUris = new ArrayList<>();
+
+    private String tokenSecret;
+
+    private long tokenExpirationMsec;
+
+
+}
